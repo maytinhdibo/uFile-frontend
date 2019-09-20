1c9f94754433654895d72fd5578dd2668af36fa3
longhoang08 <longhoang0811@gmail.com>
2019-09-20 15:53:44 +0700

    add constants folder

---
+++ src/constants/Links.js
@@ -0,0 +1,11 @@
+export const HOME = '/';
+
+export const API_REGISTER = 'register/';
+
+export const API_LOGIN = 'users/login';
+
+export const API_FETCH_USER = 'users/get_status';
+
+export const API_LOGOUT = 'users/logout';
+
+export const API_FORGOT_PASSWORD = 'profile/forgot';
---
+++ src/constants/NotificationObj.js
@@ -0,0 +1,33 @@
+export const PLEASE_REINPUT = {
+  message: "Can't submit",
+  description: 'Please make sure that the data you entered is correct format before submitting',
+};
+
+export const AUTHENTICATION_FAILED = {
+  message: 'Invalid token or signal expired',
+  description: 'Please login again',
+};
+
+export const SERVER_ERROR = {
+  message: 'Sorry',
+  description: 'Server is not available right now. Please try again later!',
+};
+
+export const SESSION_EXPIRED = {
+  message: 'Sorry',
+  description: 'Session expired. Please login again!!!',
+};
+
+export const PLEASE_UPLOAD_IMAGE = {
+  message: 'Cannot upload',
+  description: 'You can only upload files in image format',
+};
+
+export const CHANGE_AVATAR_SUCCESS = {
+  message: 'Your avatar have been changed',
+};
+
+export const CANT_CHANGE_AVATAR = {
+  message: 'Sorry',
+  description: "Can't change your avatar. Please try again later.",
+};
