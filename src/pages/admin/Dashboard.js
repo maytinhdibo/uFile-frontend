eec47365d5c191e87e5dbd9e026764fc112a8377
jian <thanhdatuetk62@gmail.com>
2019-12-23 23:38:02 +0700

    admin frontend

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,116 +1,137 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import '../../styles/admin.scss';
 
 import Modal from '../../components/common/Modal';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faLock, faInfoCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronDown, faLock, faInfoCircle, faTimes, faUnlock} from '@fortawesome/free-solid-svg-icons';
+import userServices from "../../services/users";
+import adminServices from "../../services/admin"
 
 export default class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      infoModal: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            infoModal: false,
+            users: [],
+            selectedUser: {},
+            q: '',
+            page: 1,
+            ipp: 10
+        };
+    }
+
+    componentDidMount() {
+        this.searchUser('', 1, 10);
+    }
+    searchUser = () => {
+        adminServices.searchUser(this.state.q, this.state.page, this.state.ipp).then(data => {
+            this.setState({users: data.data.users})
+        })
     };
-  }
-  render() {
-    return (
-      <div id="adm-page">
-        <header className="adm-header">
-          Administrator dashboard
-          <span className="profile">
+    logout = () => {
+        userServices.submitLogoutRequest().then(data => {
+            localStorage.clear();
+            this.props.history.push('/')
+        });
+    };
+    handleChange = e => {
+        this.setState({q: e.target.value})
+    };
+    render() {
+        return (
+            <div id="adm-page">
+                <header className="adm-header">
+                    Administrator dashboard
+                    <span className="profile">
             <a>
-              Tran Cuong
-              <span className="icon">
-                <FontAwesomeIcon icon={faChevronDown} />
+                {localStorage.fullname}
+                <span className="icon">
+                <FontAwesomeIcon icon={faChevronDown}/>
               </span>
             </a>
 
             <ul>
-              <li>Account setting</li>
-              <Link to="/">
-                <li>Logout</li>
-              </Link>
+                <li>Account setting</li>
+                <li onClick={this.logout}>Logout</li>
             </ul>
           </span>
-        </header>
-        <div className="adm-search-form  me-input">
-          <input />
-          <button>Search</button>
-        </div>
+                </header>
+                <div className="adm-search-form  me-input">
+                    <input value={this.state.q} onChange={this.handleChange}/>
+                    <button onClick={this.searchUser}>Search</button>
+                </div>
 
-        <Modal onClose={() => this.setState({ infoModal: false })} visible={this.state.infoModal}>
-          <div className="me-modal-header">
-            <h3>User detail</h3>
-            <FontAwesomeIcon
-              onClick={() => this.setState({ infoModal: false })}
-              style={{ cursor: 'pointer' }}
-              icon={faTimes}
-            />
-          </div>
-          <div>
-            <table className="admin-user-info">
-              <tr>
-                <th>Name:</th>
-                <td>Trần Cường</td>
-              </tr>
-              <tr>
-                <th>Usename:</th>
-                <td>maytinhdibo</td>
-              </tr>
-              <tr>
-                <th>Email:</th>
-                <td>iammaytinhdibo@gmail.com</td>
-              </tr>
-              <tr>
-                <th>Join at:</th>
-                <td>24/10/2019</td>
-              </tr>
-              <tr>
-                <th>Update at:</th>
-                <td>26/10/2019</td>
-              </tr>
+                <Modal onClose={() => this.setState({infoModal: false})} visible={this.state.infoModal}>
+                    <div className="me-modal-header">
+                        <h3>User detail</h3>
+                        <FontAwesomeIcon
+                            onClick={() => this.setState({infoModal: false})}
+                            style={{cursor: 'pointer'}}
+                            icon={faTimes}
+                        />
+                    </div>
+                    <div>
+                        <table className="admin-user-info">
+                            <tr>
+                                <th>Name:</th>
+                                <td>{this.state.selectedUser.fullname}</td>
+                            </tr>
+                            <tr>
+                                <th>Usename:</th>
+                                <td>{this.state.selectedUser.username}</td>
+                            </tr>
+                            <tr>
+                                <th>Email:</th>
+                                <td>{this.state.selectedUser.email}</td>
+                            </tr>
+                            <tr>
+                                <th>Join at:</th>
+                                <td>{this.state.selectedUser.created_at}</td>
+                            </tr>
+                            <tr>
+                                <th>Update at:</th>
+                                <td>{this.state.selectedUser.updated_at}</td>
+                            </tr>
 
-              <tr>
-                <th>Storage:</th>
-                <td>13/15GB</td>
-              </tr>
-            </table>
-          </div>
-        </Modal>
+                            <tr>
+                                <th>Storage:</th>
+                                <td>13/15GB</td>
+                            </tr>
+                        </table>
+                    </div>
+                </Modal>
 
-        <table border="0" className="adm-user-table">
-          <tr>
-            <th width="220">Fullname</th>
-            <th width="150">Username</th>
-            <th width="150">Email</th>
-            <th width="150">Join at</th>
-            <th width="150">Update at</th>
-            <th>Action</th>
-          </tr>
-          {[1, 21, 42, 2, 1].map(object => {
-            return (
-              <tr>
-                <td>Tran Cuong</td>
-                <td>maytinhdibo</td>
-                <td>iammaytinhdibo@gmail.com</td>
-                <td className="center">19-10-2019</td>
-                <td className="center">24-10-2019</td>
-                <td>
+                <table border="0" className="adm-user-table">
+                    <tr>
+                        <th width="220">Fullname</th>
+                        <th width="150">Username</th>
+                        <th width="150">Email</th>
+                        <th width="150">Join at</th>
+                        <th width="150">Update at</th>
+                        <th>Action</th>
+                    </tr>
+                    {this.state.users.map(user => {
+                        return (
+                            <tr>
+                                <td>{user.fullname}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td className="center">{user.created_at}</td>
+                                <td className="center">{user.updated_at}</td>
+                                <td>
                   <span className="icon">
-                    <FontAwesomeIcon icon={faLock} />
+                    <FontAwesomeIcon icon={user.is_active ? faUnlock : faLock}/>
                   </span>
-                  <span className="icon">
-                    <FontAwesomeIcon onClick={() => this.setState({ infoModal: true })} icon={faInfoCircle} />
+                                    <span className="icon">
+                    <FontAwesomeIcon onClick={() => this.setState({infoModal: true, selectedUser: user})} icon={faInfoCircle} />
                   </span>
-                </td>
-              </tr>
-            );
-          })}
-        </table>
-      </div>
-    );
-  }
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </table>
+            </div>
+        );
+    }
 }
--- src/services/admin.js
+++ src/services/admin.js
@@ -1,8 +1,12 @@
 // @flow
-import { requestServices } from 'services';
+import {requestServices} from 'services';
 
-const searchUser = (username, page, ipp) => requestServices.customAxios.get('/admin/search_users?username='+username+"&page="+page+"&ipp="+ipp).then(res => res.data);
+const searchUser = (username, page, ipp) => requestServices.customAxios.get('/admin/search_users?username=' + username + "&page=" + page + "&ipp=" + ipp).then(res => res.data);
+const blockUser = (email) => requestServices.customAxios.post('/admin/block_user', {email: email}).then(res => res.data);
+const unBlockUser = (email) => requestServices.customAxios.post('/admin/un_block_user', {email: email}).then(res => res.data);
 
 export default {
-  searchUser
+    searchUser,
+    blockUser,
+    unBlockUser
 };
